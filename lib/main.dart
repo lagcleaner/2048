a5206d4254fdc46629e523ec973eb6fe22280af1
Leonel Garcia <lagcleaner@gmail.com>
2025-03-28 13:58:04 -0400

    Setup riverpod scope to the entire app and hydrated bloc configurations

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:game2048/presentation/app.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+Future<void> main() async {
+  await initialize();
   runApp(ProviderScope(child: const TwentyFortyEightApp()));
+}
+
+Future<void> initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Orientations
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  // Path-provider
+  final directory = await getApplicationDocumentsDirectory();
+  // Init Bloc storage
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: directory,
+  );
 }
