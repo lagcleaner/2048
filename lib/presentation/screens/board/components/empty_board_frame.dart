90fc085c2a698197c4d266c8502f4252673cf9df
Leonel Garcia <lagcleaner@gmail.com>
2025-03-31 17:26:59 -0400

    Fix dimensions of the empty board frame widget

--- lib/presentation/screens/board/components/empty_board_frame.dart
+++ lib/presentation/screens/board/components/empty_board_frame.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/widgets.dart';
 import 'package:game2048/presentation/misc/theme/theme.dart';
 import 'package:game2048/presentation/models/game_mode_model.dart';
@@ -25,13 +24,13 @@ class EmptyBoardFrame extends StatelessWidget {
         borderRadius: BorderRadius.circular(6.0),
       ),
       child: Stack(
-        children: List.generate(16, (i) {
-          //Render the empty board in 4x4 GridView
-          var x = ((i + 1) / 4).ceil();
+        children: List.generate(gameMode.squaredDim, (i) {
+          //Render the empty board in dimXdim GridView
+          var x = ((i + 1) / gameMode.dimension).ceil();
           var y = x - 1;
 
           var top = y * (tileSize) + (x * 12.0);
-          var z = (i - (4 * y));
+          var z = (i - (gameMode.dimension * y));
           var left = z * (tileSize) + ((z + 1) * 12.0);
 
           return Positioned(
