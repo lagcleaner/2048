a2f6984e32f984516f845a9444c48a578a09767a
Leonel Garcia <lagcleaner@gmail.com>
2025-04-04 00:29:29 -0400

    Add game cubit implementation for full functionality

--- lib/presentation/screens/board/controllers/game_cubit.dart
+++ lib/presentation/screens/board/controllers/game_cubit.dart
@@ -1,13 +1,21 @@
-import 'dart:math' show Random;
+import 'dart:math' show Random, max, min;
 
 import 'package:flutter_swipe_detector/flutter_swipe_detector.dart';
 import 'package:game2048/presentation/models/game_mode_model.dart';
+import 'package:game2048/presentation/screens/board/controllers/next_direction_cubit.dart';
+import 'package:game2048/presentation/screens/board/controllers/round_cubit.dart';
 import 'package:game2048/presentation/screens/board/models/game_state_model.dart';
 import 'package:game2048/presentation/screens/board/models/tile_model.dart';
+import 'package:hive/hive.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:riverbloc/riverbloc.dart';
+import 'package:uuid/uuid.dart';
 
-class GameCubit extends Cubit<GameState> {
-  GameCubit([GameState? state])
+class GameCubit extends HydratedCubit<GameState> {
+  final RoundCubit round;
+  final NextDirectionCubit nextDirection;
+
+  GameCubit(this.round, this.nextDirection, [GameState? state])
     : super(
         state ??
             GameState.newGame(best: 0, tiles: [], mode: GameMode.survival()),
@@ -20,83 +28,244 @@ class GameCubit extends Cubit<GameState> {
   // which will allow us to reuse most of the logic.
   int get dim => state.mode.dimension;
 
-  void load([GameMode? mode]) async {
-    //Access the box and get the first item at index 0
-    //which will always be just one item of the Board model
-    //and here we don't need to call fromJson function of the board model
-    //in order to construct the Board model
-    //instead the adapter we added earlier will do that automatically.
-
-    // var box = await Hive.openBox<Board>('boardBox');
-    //If there is no save locally it will start a new game.
-    emit(_newGame(mode)); //box.get(0) ?? _newGame();
-  }
-
   void newGame([GameMode? mode]) {
     emit(_newGame(mode));
   }
 
-  // Check whether the indexes are in the same row or column in the board.
-  bool _inSameRowOrCol(int index, int nextIndex) {
-    return (index % state.mode.dimension == nextIndex % state.mode.dimension) ||
-        (index ~/ state.mode.dimension == nextIndex ~/ state.mode.dimension);
-  }
-
   bool move(SwipeDirection direction) {
-    return false;
-  }
+    bool asc =
+        direction == SwipeDirection.left || direction == SwipeDirection.up;
+    bool vert =
+        direction == SwipeDirection.up || direction == SwipeDirection.down;
+    // Sort the list of tiles by index.
+    // If user swipes vertically use the verticalOrder list to retrieve the up/down index
+    final sorted =
+        state.tiles.toList()..sort(
+          ((a, b) =>
+              (asc ? 1 : -1) *
+              (vert
+                  ? verticalOrder(a.index).compareTo(verticalOrder(b.index))
+                  : a.index.compareTo(b.index))),
+        );
 
-  // Generates tiles at random place on the board
-  Iterable<Tile> randomAssign(
-    List<Tile> emptyTiles,
-    int count, {
-    bool countStrict = false,
-  }) sync* {
-    if (emptyTiles.isEmpty) return;
+    List<Tile> tiles = [];
 
-    if (count > emptyTiles.length) {
-      count = emptyTiles.length;
+    for (int i = 0, l = state.tiles.length; i < l; i++) {
+      var tile = sorted[i];
+
+      // Calculate nextIndex for current tile.
+      tile = _calculate(tile, tiles, direction);
+      tiles.add(tile);
+
+      if (i + 1 < l) {
+        var next = sorted[i + 1];
+        // Assign current tile nextIndex or index to the next tile if its allowed to be moved.
+        if (tile.value == next.value) {
+          // If user swipes vertically use the verticalOrder list to retrieve the up/down index else use the existing index
+          var index = vert ? verticalOrder(tile.index) : tile.index,
+              nextIndex = vert ? verticalOrder(next.index) : next.index;
+          if (_inSameRowOrCol(index, nextIndex)) {
+            tiles.add(next.copyWith(nextIndex: tile.nextIndex));
+            // Skip next iteration if next tile was already assigned nextIndex.
+            i += 1;
+            continue;
+          }
+        }
+      }
     }
 
-    final rn = Random();
-    for (var i = 0; i < count; i++) {
-      Tile randomTile;
-      if (countStrict) {
-        randomTile = emptyTiles.removeAt(rn.nextInt(emptyTiles.length));
-      }
-      randomTile = emptyTiles.elementAt(rn.nextInt(emptyTiles.length));
-      // randomTile.value = pow(2, rn.nextInt(9) == 0 ? 2 : 1).toInt();
-      yield randomTile;
+    // Assign immutable copy of the new board state and trigger rebuild.
+    emit(state.copyWith(tiles: tiles, undo: state));
+    return true;
+  }
+
+  /// Generates tiles at random place on the board
+  Iterable<Tile> random(List<int> indexes, {int count = 1}) sync* {
+    var i = 0;
+    var rng = Random();
+    var filledIndexes = indexes.toList();
+    count = max(min(indexes.length, count), 1);
+    while (count > 0) {
+      i = rng.nextInt(state.mode.squaredDim);
+      if (filledIndexes.contains(i)) continue;
+      yield Tile(index: i, value: 2, id: Uuid().v4());
+      filledIndexes.add(i);
+      count--;
     }
     return;
   }
 
-  GameState _randomSet(int count, {bool countStrict = false}) {
-    final tiles = state.copyWith().board;
-    final emptyTiles = tiles.expand((e) => e).where((e) => e.isEmpty).toList();
+  /// Merge tiles
+  void merge() {
+    List<Tile> tiles = [];
+    var tilesMoved = false;
+    List<int> indexes = [];
+    var score = state.score;
 
-    if (emptyTiles.isEmpty) return state;
+    for (int i = 0, l = state.tiles.length; i < l; i++) {
+      var tile = state.tiles[i];
 
-    if (count > emptyTiles.length) {
-      count = emptyTiles.length;
-    }
+      var value = tile.value, merged = false;
 
-    final rn = Random();
-    for (var i = 0; i < count; i++) {
-      Tile randomTile;
-      if (countStrict) {
-        randomTile = emptyTiles.removeAt(rn.nextInt(emptyTiles.length));
+      if (i + 1 < l) {
+        //sum the number of the two tiles with same index and mark the tile as merged and skip the next iteration.
+        var next = state.tiles[i + 1];
+        if (tile.nextIndex == next.nextIndex ||
+            tile.index == next.nextIndex && tile.nextIndex == null) {
+          value = tile.value + next.value;
+          merged = true;
+          score += tile.value;
+          i += 1;
+        }
       }
-      randomTile = emptyTiles.elementAt(rn.nextInt(emptyTiles.length));
-      // randomTile.value = pow(2, rn.nextInt(9) == 0 ? 2 : 1).toInt();
+
+      if (merged || tile.nextIndex != null && tile.index != tile.nextIndex) {
+        tilesMoved = true;
+      }
+
+      tiles.add(
+        tile.copyWith(
+          index: tile.nextIndex ?? tile.index,
+          nextIndex: null,
+          value: value,
+          merged: merged,
+        ),
+      );
+      indexes.add(tiles.last.index);
     }
-    return state.copyWith(board: tiles.toList());
+
+    //If tiles got moved then generate a new tile at random position of the available positions on the board.
+    if (tilesMoved) {
+      tiles.addAll(random(indexes));
+    }
+    emit(state.copyWith(score: score, tiles: tiles));
+  }
+
+  //Finish round, win or loose the game.
+  void _endRound() {
+    var gameOver = true, gameWon = false;
+    List<Tile> tiles = [];
+
+    //If there is no more empty place on the board
+    if (state.tiles.length == state.mode.squaredDim) {
+      final sorted =
+          state.tiles.toList()..sort(((a, b) => a.index.compareTo(b.index)));
+
+      for (int i = 0, l = state.tiles.length; i < l; i++) {
+        var tile = sorted[i];
+
+        //If there is a tile with 2048 then the game is won.
+        if (tile.value == 2048) {
+          gameWon = true;
+        }
+
+        var x = (i - (((i + 1) / dim).ceil() * dim - dim));
+
+        if (x > 0 && i - 1 >= 0) {
+          //If tile can be merged with left tile then game is not lost.
+          var left = sorted[i - 1];
+          if (tile.value == left.value) {
+            gameOver = false;
+          }
+        }
+
+        if (x < (dim - 1) && i + 1 < l) {
+          //If tile can be merged with right tile then game is not lost.
+          var right = sorted[i + 1];
+          if (tile.value == right.value) {
+            gameOver = false;
+          }
+        }
+
+        if (i - dim >= 0) {
+          //If tile can be merged with above tile then game is not lost.
+          var top = sorted[i - dim];
+          if (tile.value == top.value) {
+            gameOver = false;
+          }
+        }
+
+        if (i + dim < l) {
+          //If tile can be merged with the bellow tile then game is not lost.
+          var bottom = sorted[i + dim];
+          if (tile.value == bottom.value) {
+            gameOver = false;
+          }
+        }
+        //Set the tile merged: false
+        tiles.add(tile.copyWith(merged: false));
+      }
+    } else {
+      //There is still a place on the board to add a tile so the game is not lost.
+      gameOver = false;
+      for (var tile in state.tiles) {
+        //If there is a tile with 2048 then the game is won.
+        if (tile.value == 2048) {
+          gameWon = true;
+        }
+        //Set the tile merged: false
+        tiles.add(tile.copyWith(merged: false));
+      }
+    }
+
+    emit(
+      state.copyWith(
+        tiles: tiles,
+        status: switch ((gameWon, gameOver)) {
+          (_, true) => GameStatus.over,
+          (true, _) => GameStatus.won,
+          _ => GameStatus.playing,
+        },
+      ),
+    );
+  }
+
+  //Mark the merged as false after the merge animation is complete.
+  bool endRound() {
+    //End round.
+    _endRound();
+    round.end();
+
+    //If player moved too fast before the current animation/transition finished, start the move for the next direction
+    var nextDirectionMove = nextDirection.state;
+    if (nextDirectionMove != null) {
+      move(nextDirectionMove);
+      nextDirection.clear();
+      return true;
+    }
+    return false;
+  }
+
+  //undo one round only
+  void undo() {
+    if (state.undo != null) {
+      emit(
+        state.copyWith(
+          score: state.undo!.score,
+          best: state.undo!.best,
+          tiles: state.undo!.tiles,
+        ),
+      );
+    }
+  }
+
+  int verticalOrder(int index) {
+    // dim 5 -> [20, 15, 10, 5, 0, 21, 16, 11, 6, 1, 22, 17, 12, 7, 2, 23, 18, 13, 8, 3, 24, 19, 14, 9, 4]
+    // dim 4 -> [12, 8, 4, 0, 13, 9, 5, 1, 14, 10, 6, 2, 15, 11, 7, 3];
+    // dim 3 -> [6, 3, 0, 7, 4, 1, 8, 5, 2];
+    return (dim - 1 - (index % dim)) * dim + (index ~/ dim);
+  }
+
+  int verticalOrderIndexOf(int value) {
+    return (value % dim) * dim + (dim - 1 - (value ~/ dim));
   }
 
   GameState _newGame([GameMode? mode]) {
-    final tiles = _generateBoard(mode ?? state.mode);
-    randomAssign(tiles.expand((e) => e).toList(), 2);
-    return GameState.newGame(best: state.best, board: tiles, mode: state.mode);
+    return GameState.newGame(
+      best: state.best,
+      tiles: [...random([], count: 2)],
+      mode: state.mode,
+    );
   }
 
   Tile _calculate(Tile tile, List<Tile> tiles, SwipeDirection direction) {
@@ -104,119 +273,60 @@ class GameCubit extends Cubit<GameState> {
         direction == SwipeDirection.left || direction == SwipeDirection.up;
     bool vert =
         direction == SwipeDirection.up || direction == SwipeDirection.down;
-    // Get the first index from the left in the row
-    // Example: for left swipe that can be: 0, 4, 8, 12
-    // for right swipe that can be: 3, 7, 11, 15
-    // depending which row in the column in the board we need
-    // let's say the title.index = 6 (which is the 3rd tile from the left and 2nd from right side, in the second row)
-    // ceil means it will ALWAYS round up to the next largest integer
-    // NOTE: don't confuse ceil it with floor or round as even if the value is 2.1 output would be 3.
-    // ((6 + 1) / 4) = 1.75
-    // Ceil(1.75) = 2
-    // If it's ascending: 2 * 4 – 4 = 4, which is the first index from the left side in the second row
-    // If it's descending: 2 * 4 – 1 = 7, which is the last index from the left side and first index from the right side in the second row
-    // If user swipes vertically use the verticalOrder list to retrieve the up/down index else use the existing index
-    int index = vert ? verticalOrder[tile.index] : tile.index;
-    int nextIndex = ((index + 1) / 4).ceil() * 4 - (asc ? 4 : 1);
+    int index = vert ? verticalOrder(tile.index) : tile.index;
+    int nextIndex = ((index + 1) / dim).ceil() * dim - (asc ? dim : 1);
 
-    // If the list of the new tiles to be rendered is not empty get the last tile
-    // and if that tile is in the same row as the curren tile set the next index for the current tile to be after the last tile
     if (tiles.isNotEmpty) {
       var last = tiles.last;
       // If user swipes vertically use the verticalOrder list to retrieve the up/down index else use the existing index
       var lastIndex = last.nextIndex ?? last.index;
-      lastIndex = vert ? verticalOrder[lastIndex] : lastIndex;
-      if (_inRange(index, lastIndex)) {
+      lastIndex = vert ? verticalOrder(lastIndex) : lastIndex;
+      if (_inSameRowOrCol(index, lastIndex)) {
         // If the order is ascending set the tile after the last processed tile
         // If the order is descending set the tile before the last processed tile
         nextIndex = lastIndex + (asc ? 1 : -1);
       }
     }
 
-    // Return immutable copy of the current tile with the new next index
-    // which can either be the top left index in the row or the last tile nextIndex/index + 1
     return tile.copyWith(
-      nextIndex: vert ? verticalOrder.indexOf(nextIndex) : nextIndex,
+      nextIndex: vert ? verticalOrderIndexOf(nextIndex) : nextIndex,
     );
   }
 
-  // CAN SWIPE AREA
-  bool canSwipeLeft() => state.board.any(canSwipe);
-  bool canSwipeRight() =>
-      state.board.map((e) => e.reversed.toList()).any(canSwipe);
-  bool canSwipeUp() => state.cols.any(canSwipe);
-  bool canSwipeDown() =>
-      state.cols.map((e) => e.reversed.toList()).any(canSwipe);
-  bool canSwipe(List<Tile> tiles) {
-    for (int i = 0; i <= tiles.length; i++) {
-      if (tiles[i].value == 0) {
-        if (tiles.skip(i + 1).any((e) => e.value != 0)) {
-          return true;
-        }
-      } else {
-        Tile? nextNonZero = (tiles as Iterable<Tile?>)
-            .skip(i + 1)
-            .firstWhere((e) => e?.value != 0, orElse: () => null);
-        if (nextNonZero?.value == tiles[i].value) return true;
-      }
-    }
-    return false;
+  /// Check whether the indexes are in the same row or column in the board.
+  bool _inSameRowOrCol(int index, int nextIndex) {
+    return (index % dim == nextIndex % dim) ||
+        (index ~/ dim == nextIndex ~/ dim);
   }
 
-  // SWIPE AREA
-  void doSwipeLeft() => state.board.map(mergeTiles);
-  void doSwipeRight() =>
-      state.board.map((e) => e.reversed.toList()).map(mergeTiles);
-  void doSwipeUp() => state.cols.map(mergeTiles);
-  void doSwipeDown() =>
-      state.cols.map((e) => e.reversed.toList()).map(mergeTiles);
-  void mergeTiles(List<Tile> tiles) {
-    for (int i = 0; i <= tiles.length; i++) {
-      Iterable<Tile> toCheck = tiles.skip(i).skipWhile((t) => t.isEmpty);
-      if (toCheck.isNotEmpty) {
-        var tile = toCheck.first;
-        var merge = toCheck.skip(1).skipWhile((e) => e.isEmpty).firstOrNull;
-        if (merge != null && tile.value != merge.value) {
-          merge = null;
-        }
-        if (tiles[i] != tile || merge != null) {
-          int resultValue = tile.value;
-          if (merge != null) {
-            resultValue += merge.value;
-            // merge.value = 0;
-          }
-          // tile.value = 0;
-          // tiles[i].value = resultValue;
-        }
-      } else {
-        break;
-      }
+  void load() async {
+    var box = await Hive.openBox<GameState>('gameStateBox');
+    emit(box.get(0) ?? _newGame());
+  }
+
+  void save() async {
+    var box = await Hive.openBox<GameState>('gameStateBox');
+    try {
+      box.putAt(0, state);
+    } catch (e) {
+      box.add(state);
     }
   }
 
-  Tile _calculate(Tile tile, List<Tile> tiles, direction) {
-    // Get the first index from the left in the row
-    // Example: for left swipe that can be: 0, 4, 8, 12
-    // depending which row in the column in the board we need
-    int index = tile.index;
-    int nextIndex = ((index + 1) / 4).ceil() * 4 - 4;
+  @override
+  GameState? fromJson(Map<String, dynamic> json) {
+    return GameState.fromJson(json);
+  }
 
-    // If the list of the new tiles to be rendered is not empty get the last tile
-    // and if that tile is in the same row as the curren tile set the next index for the current tile to be after the last tile
-    if (tiles.isNotEmpty) {
-      var last = tiles.last;
-      var lastIndex = last.nextIndex ?? last.index;
-      if (_inSameRowOrCol(index, lastIndex)) {
-        nextIndex = lastIndex + 1;
-      }
-    }
-
-    // Return immutable copy of the current tile with the new next index
-    // which can either be the top left index in the row or the last tile nextIndex/index + 1
-    return tile.copyWith(nextIndex: nextIndex);
+  @override
+  Map<String, dynamic>? toJson(GameState state) {
+    return state.toJson();
   }
 }
 
 final gameCubitProvider = BlocProvider<GameCubit, GameState>((ref) {
-  return GameCubit();
+  return GameCubit(
+    ref.read(roundCubit.bloc),
+    ref.read(nextDirectionCubit.bloc),
+  );
 });
