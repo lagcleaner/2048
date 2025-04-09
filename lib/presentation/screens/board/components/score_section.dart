58864fbb7284beed9b9da2982245501bf2bb9b85
Leonel Garcia <lagcleaner@gmail.com>
2025-04-09 01:25:08 -0400

    Receive scores updates by game cubit changes

--- lib/presentation/screens/board/components/score_section.dart
+++ lib/presentation/screens/board/components/score_section.dart
@@ -2,21 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:game2048/presentation/components/gaps.dart';
 import 'package:game2048/presentation/components/score.dart';
+import 'package:game2048/presentation/screens/board/controllers/game_cubit.dart';
 
 class ScoreSection extends ConsumerWidget {
   const ScoreSection({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // final score = ref.watch(boardManager.select((board) => board.score));
-    // final best = ref.watch(boardManager.select((board) => board.best));
+    final score = ref.watch(gameCubitProvider.select((board) => board.score));
+    final best = ref.watch(gameCubitProvider.select((board) => board.best));
     final scores = <Widget>[
-      Score(
-        label: 'Best',
-        score: '8978996',
-        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      ),
-      Score(label: 'Score', score: '12314'),
+      Score(label: 'Best', score: '$best'),
+      Score(label: 'Score', score: '$score'),
     ];
     //
     return MediaQuery.of(context).size.width < 360
